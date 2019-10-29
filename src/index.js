face319be5daccf0c09862dbba45d6800b952622
github username <github email address>
2019-10-29 14:39:41 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+	// your code here
+	addComments();
 });
+
+
+
+function addComments() {
+	const taskForm = document.getElementById('create-task-form');
+
+	taskForm.addEventListener('submit', function(event) {
+    		event.preventDefault();
+
+
+		const inputBox = event.target.querySelector('#new-task-description');
+		const theList = document.querySelector('#tasks');
+		const userInput = document.createElement('li');
+		
+		userInput.textContent = inputBox.value;
+    		theList.appendChild(userInput);
+
+	});
+}
